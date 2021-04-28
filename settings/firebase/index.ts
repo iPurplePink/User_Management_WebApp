71e718615b0349ee71197013dba6699c3afad4ab
Maridin San Miguel <maridin.sanmiguel@ubidy.com>
2021-04-28 13:24:00 +0800

    Update environment variables

--- settings/firebase/index.ts
+++ settings/firebase/index.ts
@@ -5,10 +5,10 @@ import "firebase/analytics";
 import "firebase/auth";
 import "firebase/firestore";
 
-const PROJECT_ID = process.env.FIREBASE_PROJECT_ID;
-const API_KEY = process.env.FIREBASE_API_KEY;
-const APP_ID = process.env.FIREBASE_APP_ID;
-const G_MEASUREMENT_ID = process.env.FIREBASE_G_MEASUREMENT_ID;
+const PROJECT_ID = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
+const API_KEY = process.env.NEXT_PUBLIC_FIREBASE_API_KEY;
+const APP_ID = process.env.NEXT_PUBLIC_FIREBASE_APP_ID;
+const G_MEASUREMENT_ID = process.env.NEXT_PUBLIC_FIREBASE_G_MEASUREMENT_ID;
 const SENDER_ID = process.env.FIREBASE_SENDER_ID;
 
 var firebaseConfig = {
